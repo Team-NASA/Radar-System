746a44099e5ffbeb0e65a4b9b245a30fdf5baf78
StevefromSpace <iamstevefromspace@gmail.com>
2026-03-02 14:10:52 +0530

    refactor: Changed and updated the logic and change in UI

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-// --- WIRED CONFIGURATION ---
 const wiredPort = 'COM10'; 
 const baudRate = 9600; 
 
@@ -21,16 +20,13 @@ const port = new SerialPort({
 
 const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
 
-// --- SERVE THE DASHBOARD ---
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// --- DATA HANDLING ---
 port.on('open', () => console.log(`✅ USB Connected on ${wiredPort}`));
 
 parser.on('data', (data) => {
-    // Sends "angle,distance" to the web dashboard
     io.emit('radarData', data); 
 });
 
